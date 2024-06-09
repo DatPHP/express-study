658c74425d48d87d44c9517e2a167f9dbcc1f8a1
DatPHP <nguyenvandat170296@gmail.com>
2024-06-09 15:40:49 +0700

    Complete NodeJS Express and SQL

--- index.js
+++ index.js
@@ -4,6 +4,37 @@ app.use(express.static('public'));
 var path = require('path');
 var fs = require("fs");
 
+var mysql = require('mysql');
+var con = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "",
+   database: "mydb123"
+});
+
+var qry =`
+SELECT 
+   m.member_id, 
+   m.name AS member, 
+   c.committee_id, 
+   c.name AS committee
+FROM
+   members m
+INNER JOIN committees c ON c.name = m.name;
+`;
+con.connect(function (err) {
+   if (err) throw err;
+   console.log("Connected!");
+   con.query(qry, function (err, results) {
+      if (err) throw err;
+      results.forEach((row) => {
+         console.log(JSON.stringify(row));
+      });
+   });
+
+   con.end();
+});
+
 var bodyParser = require('body-parser');
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
